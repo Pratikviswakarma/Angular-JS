1b32a369fb80fe4cd741c403c2439c0eefb540a4
Pratikviswakarma <pratikvishwakarma1812@gmail.com>
2025-03-09 14:47:15 +0530

    [Pratik] Add. Logo using property binding technique

--- HelloworldApp/src/app/app.component.ts
+++ HelloworldApp/src/app/app.component.ts
@@ -4,21 +4,14 @@ import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterModule,FormsModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title="Helloworld";
   imgUrl: string = './assets/logo.jpg';
-  url="https://www.bridgelabz.com";
- 
+   
   ngOnInit():void{
     this.title="Hello from Bridgelabz";
-  }
-
-  onClick(event: Event) {
-    console.log("Save button is clicked! ");
-    window.open(this.url,"_blank")
   }
 }
